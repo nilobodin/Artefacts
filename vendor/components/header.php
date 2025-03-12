bf828bfc3d33d775e1c1f1e76f9d317097b7b7e2
nilobodin <nikita.lobodin2005@mail.ru>
2025-03-12 13:49:11 +0600

    Divided the main page into components such as header, main and footer

---
+++ vendor/components/header.php
@@ -0,0 +1,35 @@
+<header class="header">
+    <div class="container">
+        <div class="header__row">
+            <div class="header__row_logo">
+                <a href="#" class="header__row_logo-link">
+                    <img src="assets/img/logo.png" alt="not founded" class="header__row_img">
+                </a>
+            </div>
+            <nav class="header__row_nav">
+                <div class="header__row_list">
+                    <a href="" class="header__row_link">
+                        <li class="header__row_li">Каталог</li>
+                    </a>
+                    <a href="" class="header__row_link">
+                        <li class="header__row_li">Галерея</li>
+                    </a>
+                    <a href="" class="header__row_link">
+                        <li class="header__row_li">О лаборатории</li>
+                    </a>
+                    <a href="" class="header__row_link">
+                        <li class="header__row_li">Контакты</li>
+                    </a>
+                </div>
+            </nav>
+            <div class="header__row_btns">
+                <div class="header__row_search">
+                    <img src="assets/img/search.svg" alt="not founded" class="header__row_search-img">
+                </div>
+                <button class="header__row_logout">
+                    <img src="assets/img/log-in.svg" alt="not founded" class="header__row_logout-img">
+                </button>
+            </div>
+        </div>
+    </div>
+</header>
